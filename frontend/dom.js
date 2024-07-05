814707158b473ed4aa31f2c9f70767d924d389b9
camila <163159061+camila11ccmaldonado@users.noreply.github.com>
2024-07-05 13:48:36 +0200

    admin

--- frontend/dom.js
+++ frontend/dom.js
@@ -19,6 +19,22 @@ function changeScreen() {
         titulo.style.display = "none";
     }
 }
+
+function changeScreenAdmin() {
+    const notepad = document.getElementById("notepad");
+    const login = document.getElementById("login");
+    const loginAdmin = document.getElementById("loginAdmin");
+    if(notepad.style.display !== "none") {
+        notepad.style.display = "none";
+        login.style.display = "";
+        loginAdmin.style.display = "";
+    }
+    else {
+        notepad.style.display = "";
+        login.style.display = "none";
+        loginAdmin.style.display = "none";
+    }
+}
 function getPreguntaContent() {
     return document.getElementById("content").value;
 }
